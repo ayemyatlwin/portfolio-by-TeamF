75d07b3dba80828c038858c0be91d37a9fe730d5
Hannah <erikahn05@gmail.com>
2023-07-06 22:00:15 +0630

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <h1>Team F</h1> 
+      <p>portfolio project</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/TestPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestPage = () => {
+  return (
+    <div>
+     asdffdsa
+    </div>
+  )
+}
+
+export default TestPage
