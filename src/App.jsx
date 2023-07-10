f00748cfe30ca69ddd10f4b3dae305fdfb09b5a1
Hannah <erikahn05@gmail.com>
2023-07-10 22:38:30 +0630

    service

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ import Testimonial from "./Pages/Testimonial";
 import Weapons from "./Pages/Weapons";
 import StayConnected from "./Pages/StayConnected";
 import ContactMe from "./Pages/ContactMe";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
 
 const App = () => {
   return (
-    <div className="overflow-hidden screenScroll scrollbar-thin scrollbar-thumb-rose-500 scrollbar-track-slate-300">
+    <div className=" scrollbar-thin scrollbar-thumb-rose-500 scrollbar-track-slate-300">
+      <About/>
+      <Services/>
+      {/* တခုကျန် right here */}
       <Testimonial />
       <Weapons />
       <ContactMe />
